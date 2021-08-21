f49a26bee3c2e5a9197711f6516e228c607a5d94
MatildaMared <matildamared@live.se>
2021-08-21 14:46:27 +0200

    remove unnecessary code

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,29 +3,14 @@ import { PokemonContext } from "../PokemonContext";
 import "./SearchBar.scss";
 
 function SearchBar() {
-	const { context, updateContext } = useContext(PokemonContext);
+	const { updateContext } = useContext(PokemonContext);
 	const [input, setInput] = useState("");
 
 	const handleInput = (e) => {
 		setInput(e.target.value);
 	};
 
-	const filterPokemon = (input) => {
-		if (input.length === 0) {
-			updateContext({
-				filteredPokemon: context.pokemon,
-			});
-		} else {
-			updateContext({
-				filteredPokemon: context.pokemon.filter((character) => {
-					return character.name.toLowerCase().includes(input.toLowerCase());
-				}),
-			});
-		}
-	};
-
 	useEffect(() => {
-		// filterPokemon(input);
 		updateContext({
 			filterString: input,
 		});
